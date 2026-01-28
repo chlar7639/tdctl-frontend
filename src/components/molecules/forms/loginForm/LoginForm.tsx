b960df47ffad9ae0f151f889e633d2676f6b73e9
chlar7639 <chlar7639@uit.no>
2026-01-28 17:35:31 +0100

    changed color of button

--- src/components/molecules/forms/loginForm/LoginForm.tsx
+++ src/components/molecules/forms/loginForm/LoginForm.tsx
@@ -16,36 +16,38 @@ export interface LoginFormProps {
   shouldRegister?: boolean;
 }
 
-const EyeIcon = () => (
+
+const EyeIcon = ({ color = "#c2c2c2" }) => (
   <svg
     width="16"
     height="16"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke={color}
     strokeWidth="2"
     strokeLinecap="round"
-    strokeLinejoin="round">
+    strokeLinejoin="round"
+  >
     <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
     <circle cx="12" cy="12" r="3" />
   </svg>
 );
 
-const EyeOffIcon = () => (
+const EyeOffIcon = ({ color = "#c2c2c2" }) => (
   <svg
     width="16"
     height="16"
     viewBox="0 0 24 24"
     fill="none"
-    stroke="currentColor"
+    stroke={color}
     strokeWidth="2"
     strokeLinecap="round"
-    strokeLinejoin="round">
+    strokeLinejoin="round"
+  >
     <path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24" />
     <line x1="1" y1="1" x2="23" y2="23" />
   </svg>
 );
-
 const LoginForm: React.FC<LoginFormProps> = ({
   shouldRedirect = true,
   shouldRegister = true,
@@ -105,14 +107,14 @@ const LoginForm: React.FC<LoginFormProps> = ({
               label={'Passord'}
               type={showPassword ? 'text' : 'password'}
               onChange={onFieldChange}
+            
             />
+            
             <IconButton
-              size="sm"
-              variant="ghost"
               onClick={togglePasswordVisibility}
               type="button"
-              aria-label={showPassword ? 'Skjul passord' : 'Vis passord'}
-              icon={showPassword ? <EyeOffIcon /> : <EyeIcon />}
+              aria-label={showPassword ? 'Vis passord' : 'Skjul passord'}
+              icon={showPassword ? <EyeIcon /> : <EyeOffIcon />}
               className="passwordToggle"
             />
           </div>
